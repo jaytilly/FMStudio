255db213c85ef309597cbe04e3b10ba959f56fca
Marco van den Oever <marco@flyingpie.nl>
2015-06-14 16:52:37 +0200

    Fixed #16

---
+++ FMStudio.Lib.Test.Migrations/Migrations/0007_MigrationWithAReallyReallyLoooonngggName.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMStudio.Lib.Test.Migrations.Migrations
+{
+    [Migration(7, "Migration with a really really loooonnggg name")]
+    public class MigrationWithAReallyReallyLoooonngggName : Migration
+    {
+        public override void Up()
+        {
+            
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FMStudio.Lib.Test.Migrations/Resources/0007_MigrationWithAReallyReallyLoooonngggName.sql
@@ -0,0 +1,1 @@
+﻿-- 0007_MigrationWithAReallyReallyLoooonngggName.sql

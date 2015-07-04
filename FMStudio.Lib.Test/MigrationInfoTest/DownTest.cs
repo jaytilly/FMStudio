b85af146e6596c706848a765ec5cd6d3faeb487f
Marco vd Oever <marco@flyingpie.nl>
2015-07-04 15:48:15 +0200

    Added test files

---
+++ FMStudio.Lib.Test/MigrationInfoTest/DownTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FMStudio.Lib.Test.MigrationInfoTest
+{
+    [TestClass]
+    public class DownTest
+    {
+        [TestMethod]
+        public void MigrationInfo_Down_Success()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MigrationInfo_Down_AlreadyRan()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
---
+++ FMStudio.Lib.Test/ProfileInfoTest/RunProfileTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FMStudio.Lib.Test.ProfileTest
+{
+    [TestClass]
+    public class RunProfileTest
+    {
+        [TestMethod]
+        public void ProfileInfo_Run_Success()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}

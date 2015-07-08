0c21ca4a791b0a3d7beba0071cdaa345ff208a82
Marco van den Oever <marco@flyingpie.nl>
2015-07-08 22:09:37 +0200

    Fixed #36: 'Save to file...' for profiles

--- FMStudio.App/ViewModels/ProfileViewModel.cs
+++ FMStudio.App/ViewModels/ProfileViewModel.cs
@@ -2,6 +2,7 @@
 using FMStudio.Lib;
 using FMStudio.Utility.Logging;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -14,10 +15,12 @@ namespace FMStudio.App.ViewModels
         public ProfilesViewModel ProfilesVM { get; set; }
 
         public ProfileInfo ProfileInfo { get; set; }
-        
+
         public Binding<string> Sql { get; private set; }
 
-        public ICommand RunProfileCommand { get; set; }
+        public ICommand RunProfileCommand { get; private set; }
+
+        public ICommand SaveToFileCommand { get; private set; }
 
         public ProfileViewModel(ILog log, ProfilesViewModel profilesVM, ProfileInfo profileInfo)
         {
@@ -25,10 +28,11 @@ namespace FMStudio.App.ViewModels
 
             ProfilesVM = profilesVM;
             ProfileInfo = profileInfo;
-            
+
             Sql = new Binding<string>(() => ProfileInfo.GetSqlAsync().Result); // TODO: Make nicer
 
             RunProfileCommand = new RelayCommand(async param => await RunProfileAsync());
+            SaveToFileCommand = new RelayCommand(param => SaveToFile());
         }
 
         public override async Task InitializeAsync()
@@ -56,6 +60,28 @@ namespace FMStudio.App.ViewModels
             catch (Exception e)
             {
                 _log.Error("Could not run profile '{0}': {1}", ProfileInfo.Name, e.GetFullMessage());
+            }
+        }
+
+        private void SaveToFile()
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+
+            dialog.DefaultExt = ".sql";
+            dialog.Filter = "SQL files (*.sql)|*.sql|All Files (*.*)|*.*";
+
+            var result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                try
+                {
+                    File.WriteAllText(dialog.FileName, Sql.Value);
+                }
+                catch (Exception e)
+                {
+                    _log.Error("Error while writing to file: {0}", e.GetFullMessage());
+                }
             }
         }
     }

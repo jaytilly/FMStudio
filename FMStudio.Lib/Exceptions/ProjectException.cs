ef0b71d30d75d0a30a34297bf2c2e2ac59541792
Marco vd Oever <marco@flyingpie.nl>
2015-07-08 07:22:54 +0200

    Build fix

---
+++ FMStudio.Lib/Exceptions/ProjectException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FMStudio.Lib.Exceptions
+{
+    public class ProjectException : Exception
+    {
+        public ProjectInfo ProjectInfo { get; private set; }
+
+        public ProjectException(string message, Exception innerException, ProjectInfo projectInfo)
+            : base(message, innerException)
+        {
+            ProjectInfo = projectInfo;
+        }
+    }
+}

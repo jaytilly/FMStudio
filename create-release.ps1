324916a33501dbacbb90ca36c6a6c6f4bb8f4dc3
Marco van den Oever <marco@flyingpie.nl>
2015-07-15 06:14:32 +0200

    Added version parameter to create-package command

--- create-release.ps1
+++ create-release.ps1
@@ -1,7 +1,11 @@
+param (
+	[string]$Version
+)
+
 $nuget = ".\.nuget\NuGet.exe"
 $squirrel = "packages\squirrel.windows.0.99.1.1\tools\squirrel.exe"
 
-& $nuget pack FMStudio.nuspec
+& $nuget pack FMStudio.nuspec -Version $version
 
 $nupkg = (gci *.nupkg)[0]
 

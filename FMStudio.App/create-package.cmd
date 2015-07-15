de348a8580411474d3ba8043a7b719389cfe8c2d
Marco van den Oever <marco@flyingpie.nl>
2015-07-15 19:15:34 +0200

    Renamed FMStudio.App.exe to FMStudio.exe

--- FMStudio.App/create-package.cmd
+++ FMStudio.App/create-package.cmd
@@ -1,7 +1,7 @@
 rmdir package /s /q
 mkdir package
 
-xcopy /I /Y FMStudio.App.exe package
+xcopy /I /Y *.exe package
 xcopy /I /Y *.dll package
 xcopy /I /Y /S x64 "package\x64"
 xcopy /I /Y /S x86 "package\x86"

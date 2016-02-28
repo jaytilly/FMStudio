ca98798098d796b9a0186a8866021754d5147560
Marco vd Oever <marco@flyingpie.nl>
2016-02-28 15:00:07 +0100

    Added Context.cs

---
+++ FMStudio.Lib.Test/Context.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace FMStudio.Lib.Test
+{
+    public class Context
+    {
+        public string Name { get; set; }
+
+        public string DBConnectionString { get; set; }
+
+        public string FMTestMigrationsPath { get; set; }
+
+        // Used for testing non-migration dll's
+        public string FMUtilityDllPath { get; set; }
+
+        public string FMDllPath { get; set; }
+
+        public string FMRunnerDllPath { get; set; }
+
+        public string FMTestMigrationsFullName { get; set; }
+
+        public Context(string name)
+        {
+            Name = name;
+        }
+
+        public Context Initialize()
+        {
+            using (var conn = new SqlConnection(DBConnectionString))
+            {
+                conn.Open();
+
+                var comm = new SqlCommand(@"
+IF OBJECT_ID('dbo.VersionInfo', 'U') IS NOT NULL
+    DROP TABLE dbo.VersionInfo
+", conn);
+                comm.ExecuteNonQuery();
+            }
+
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class ContextProvider : IEnumerable<object[]>
+    {
+        IEnumerator<object[]> IEnumerable<object[]>.GetEnumerator()
+        {
+            yield return new[]
+            {
+                new Context("FM160 SQL2014")
+                {
+                    DBConnectionString = @"server=.\SQL2014;database=FMStudioTestDb;integrated security=true;",
+
+                    FMTestMigrationsPath = Path.GetFullPath(@"FMStudio.Lib.Test.Migrations.dll"),
+                    FMUtilityDllPath = Path.GetFullPath(@"FMStudio.Utility.dll"),
+                    FMDllPath = Path.GetFullPath(@"FluentMigrator.dll"),
+                    FMRunnerDllPath = Path.GetFullPath(@"FluentMigrator.Runner.dll"),
+                    FMTestMigrationsFullName = "FMStudio.Lib.Test.Migrations, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"
+                }.Initialize()
+            };
+            yield return new[]
+            {
+                new Context("FM130 SQL2014")
+                {
+                    DBConnectionString = @"server=.\SQL2014;database=FMStudioTestDb;integrated security=true;",
+
+                    FMTestMigrationsPath = Path.GetFullPath(@"../../FMStudio.Lib.Test.Migrations.FM130/bin/FMStudio.Lib.Test.Migrations.FM130.dll"),
+                    FMUtilityDllPath = Path.GetFullPath(@"FMStudio.Utility.dll"),
+                    FMDllPath = Path.GetFullPath(@"FluentMigrator.dll"),
+                    FMRunnerDllPath = Path.GetFullPath(@"FluentMigrator.Runner.dll"),
+                    FMTestMigrationsFullName = "FMStudio.Lib.Test.Migrations.FM130, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"
+                }.Initialize()
+            };
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
